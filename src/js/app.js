7902d2d91e10f7f94beca966bd3e9095baa139f2
Viktor Perdomo <vperdomo@orangetheoryfitness.com>
2017-10-18 14:14:17 -0400

    New Update

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 require('../css/style.css');
+const WebFont = require('webfontloader');
 const people = require('../js/people.js');
 const $      = require('jQuery');
 
-
+// Array with names to show.
 $.each(people, (key, val) => {
 	$('body').append(`<h1> ${people[key].name} </h1>`);
 });
 
 
 
-console.group(people[1].name);
+console.log(people[1].name);
