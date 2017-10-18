e25c65d25cdff7d7f2791d7694fdf6e4e7096dab
Viktor Perdomo <vperdomo@orangetheoryfitness.com>
2017-10-18 14:20:25 -0400

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	module:{
 		loaders:[
 			{ test:/\.css$/, loader:'style-loader!css-loader' }	,
-			{ test:/\.js$/, loader:'babel-loader', exclude: /node_modules/, query:{ presets:['es2015']} }	
+			{ test:/\.js$/, loader:'babel-loader', exclude: [/node_modules/, /dist/],  query:{ presets:['es2015']} }	
 		]
 	}
 }
